9fe2acc776f1b0914d1070fff15eb00498a0f8cf
Yangesi <yangesi123@gmail.com>
2024-05-13 10:25:48 -0600

    Cambios de los modelos residencia, municipio y el storecontext

---
+++ APILibros/Models/Residencia.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace APILibros.Models
+{
+    public class Residencia
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Codigo { get; set; } // Clave primaria autoincremental
+
+        [Required]
+        public int CodigoPais { get; set; } // Código del país
+
+        [Required]
+        public int CodigoDepartamento { get; set; } // Código del departamento
+
+        [Required]
+        public int CodigoMunicipio { get; set; } // Código del municipio
+
+        [ForeignKey("CodigoPais, CodigoDepartamento, CodigoMunicipio")]
+        public Municipio Municipio { get; set; } // Propiedad de navegación a Municipio
+
+        [Required]
+        [StringLength(100)]
+        public string Direccion { get; set; } // Dirección
+
+        [Required]
+        [StringLength(100)]
+        public string Referencia { get; set; } // Referencia
+
+    }
+}
--- APILibros/Models/StoreContext.cs
+++ APILibros/Models/StoreContext.cs
@@ -12,6 +12,8 @@ namespace APILibros.Models
         public DbSet<Pais> Paises { get; set; }
         public DbSet<Departamento> Departamentos { get; set; }
         public DbSet<Municipio> Municipios { get; set; }
+        public DbSet<Residencia> Residencias { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -31,13 +33,23 @@ namespace APILibros.Models
             // Asegurarse que las relaciones de navegación están correctamente configuradas
             modelBuilder.Entity<Departamento>()
                 .HasOne(d => d.Pais)
-                .WithMany(p => p.Departamentos)
+                .WithMany(p => p.Departamentos) //Departamentos es la lista que esta en pais
                 .HasForeignKey(d => d.CodigoPais);
 
             modelBuilder.Entity<Municipio>()
                 .HasOne(m => m.Departamento)
                 .WithMany(d => d.Municipios)
                 .HasForeignKey(m => new { m.CodigoPais, m.CodigoDepartamento });
+
+            modelBuilder.Entity<Residencia>()
+                 .HasOne(m => m.Municipio)
+                 .WithMany(d => d.Residencias)
+                 .HasForeignKey(m => new { m.CodigoPais, m.CodigoDepartamento, m.CodigoMunicipio });
+
+            // Definición de la restricción única para Residencia
+            modelBuilder.Entity<Residencia>()
+                .HasIndex(r => new { r.Direccion, r.CodigoPais, r.CodigoDepartamento, r.CodigoMunicipio })
+                .IsUnique();
         }
     }
 }
